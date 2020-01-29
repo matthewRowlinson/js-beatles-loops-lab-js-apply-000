ce6d2d8bc73a46b792b8b0d796ca402be953a6d0
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-01-29 20:43:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function iLoveTheBeatles(number){
   var i = 0;
   var beatles = Array();
   do{
-    beatles[i] = "I love the Beatles!";
+    beatles[i] = "I love the Beatles!\n";
     i++;
   }
   while(i < 15);
