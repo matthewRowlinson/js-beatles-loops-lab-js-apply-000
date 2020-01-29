782fde305f78b9696981c82ed29ff95878514228
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-01-29 19:48:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+```function theBeatlesPlay(musicians, instruments){
+  var array;
+  for (let i = 0; i < musicians.length; i++){
+    array[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return array;
+}
+```
+function johnLennonFacts(facts){
+  var i = 0; 
+  while(facts[i]){
+    facts[i] += "!!!";
+    i++;
+  }
+}
+```
+function iLoveTheBeatles(number){
+  var i = 0;
+  do{
+    iLoveTheBeatles[i] = "I love the Beatles!";
+    i++;
+  }
+  while(i < 15);
+}
+```
