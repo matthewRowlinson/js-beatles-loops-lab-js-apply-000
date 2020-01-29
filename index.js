fc0cc3b2a056cc1d62d696103caf2dcc6f3a41ce
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-01-29 20:40:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   let i = 0; 
-  while(i < facts.length){
+  while(facts[i]){
     facts[i].concat("!!!");
     i++;
   }
