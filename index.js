b399348c3d9fbd98058d9b20a3258b65d15f6fd5
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-01-29 20:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 ```function theBeatlesPlay(musicians, instruments){
   var array;
-  for (let i = 0; i < musicians.length; i++){
+  for (let i = 0; i < musicians.length - 1; i++){
     array[i] = musicians[i] + " plays " + instruments[i];
   }
   return array;
